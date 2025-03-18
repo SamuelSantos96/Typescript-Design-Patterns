7026bfefe76fa435f70bfaad4bb531b94cba6a0f
Samuel Santos <samuel.trabalho.96@gmail.com>
2025-03-18 01:17:34 +0000

    Add Adapter Decorator.

---
+++ Adapter/adapter.ts
@@ -0,0 +1,11 @@
+class IPhone7 {
+    useLightning() {
+        console.log("Using lightning port...");
+    }
+}
+
+class GooglePixel {
+    useMicroUSB() {
+        console.log("Using micro USB...");
+    }
+}
