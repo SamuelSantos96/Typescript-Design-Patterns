c615430a66c5e17a996f52f3ee12c032cce07f3e
Samuel Santos <samuel.trabalho.96@gmail.com>
2025-03-18 01:29:44 +0000

    Update Adapter Decorator.

--- Adapter/adapter.ts
+++ Adapter/adapter.ts
@@ -1,11 +1,37 @@
-class IPhone7 {
-    useLightning() {
+interface IPhone {
+    useLightning();
+}
+
+interface Android {
+    useMicroUsb();
+}
+
+class IPhone7 implements IPhone {
+    public useLightning() {
         console.log("Using lightning port...");
     }
 }
 
-class GooglePixel {
-    useMicroUSB() {
+class GooglePixel implements Android {
+    public useMicroUsb() {
         console.log("Using micro USB...");
     }
 }
+
+class LightningToMicroUsbAdapter implements Android {
+    iphoneDevice = new IPhone7();
+
+    constructor(iphoneDevice: IPhone7) {
+        this.iphoneDevice = iphoneDevice;
+    }
+
+    public useMicroUsb() {
+        console.log("I want to use micro USB, converting to Lightning...");
+        this.iphoneDevice.useLightning();
+    }
+}
+
+let iphone = new IPhone7();
+let chargeAdapter = new LightningToMicroUsbAdapter(iphone);
+
+chargeAdapter.useMicroUsb(); 
