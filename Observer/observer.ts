6fb9e31df9082b53d1500cf4fc57142381a1879f
Samuel Santos <samuel.trabalho.96@gmail.com>
2025-03-18 00:59:09 +0000

    Update Observer Decorator. Update Scripts.

--- Observer/observer.ts
+++ Observer/observer.ts
@@ -1,12 +1,71 @@
-class WeatherStation {
+interface Subject {
+    registerObserver(obs: Observer);
+    removeObserver(obs: Observer);
+    notifyObservers();
+}
+
+interface Observer {
+    update(temperature: number);
+}
+
+class WeatherStation implements Subject {
     private temperature: number;
+    private observers: Observer[] = [];
 
     setTemperature(temp: number) {
         console.log("WeatherStation: new temperature measurement:", temp);
         this.temperature = temp;
+        this.notifyObservers();
+    }
+
+    registerObserver(obs: Observer) {
+        this.observers.push(obs);
+    }
+
+    removeObserver(obs: Observer) {
+        let index = this.observers.indexOf(obs);
+        this.observers.splice(index, 1);
+    }
+
+    notifyObservers() {
+        this.observers.map((obs) => {
+            obs.update(this.temperature);
+        })
     }
 }
 
-class TemperatureDisplay {
-    
+class TemperatureDisplay implements Observer {
+    private subject: Subject;
+
+    constructor(weatherStation: Subject) {
+        this.subject = weatherStation;
+        weatherStation.registerObserver(this)
+    }
+
+    update(temperature: number) {
+        console.log("TemperatureDisplay:", temperature);
+    }
 }
+
+class Fan implements Observer {
+    private subject: Subject;
+
+    constructor(weatherStation: Subject) {
+        this.subject = weatherStation;
+        weatherStation.registerObserver(this)
+    }
+
+    update(temperature: number) {
+        if (temperature > 25) console.log("Fan: It's hot here, turning myself on...");
+        else console.log("Fan: It's nice and cool, turning myself off...");
+    }
+}
+
+let weatherStation = new WeatherStation();
+
+let tempDisplay = new TemperatureDisplay(weatherStation);
+let fan = new Fan(weatherStation);
+
+weatherStation.setTemperature(20);
+weatherStation.setTemperature(30);
+
